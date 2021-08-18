4186b101e024da01dadbbb7fe3d9b645e002fab3
kapilsahu <kapilsahu232@gmail.com>
2021-08-18 14:49:13 +0530

    Deploy app to github

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
       .catch((error) => console.log("error"));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
 
@@ -29,9 +33,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMosters} />
       </div>
